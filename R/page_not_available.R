0a725df96189bad7f39574cc08b82a5b9a089e17
Holly Kirk <hollylkirk@gmail.com>
2018-11-23 09:43:24 +1100

    minor edit to the "page not available" message

--- R/page_not_available.R
+++ R/page_not_available.R
@@ -1,7 +1,7 @@
 # if we don't have that page, tell the user and prompt them to submit one
 page_not_available <- function (namespace, fun) {
 
-  msg <- sprintf("we don't have a page for %s::%s yet",
+  msg <- sprintf("we don't have a page for %s::%s yet. You can make one by using create_page()",
                  namespace, fun)
   message(msg)
 
