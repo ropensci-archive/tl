c4a6e5ed341a92889bd8a67f42e7cead70ff99ea
Lingtax <m.ling@deakin.edu.au>
2018-11-22 13:02:49 +1100

    implements parsing process for dr()

---
+++ R/dr.R
@@ -0,0 +1,28 @@
+dr <- function(fun, namespace = NULL){
+  warn = FALSE
+  # if namespace == NULL & !grep("::", fun) search path
+
+  # If !is.null(namespace) query
+  if(!is.null(namespace)){
+    f <-  fun
+    ns <-  namespace
+  }
+
+  # If fun contains namespace, return from specified namespace
+    #if !is.null(namespace) warn
+  if(grepl("::", fun)) {
+    if(!is.null(namespace)) warn = TRUE
+    x <- strsplit(fun, "::")
+    f <- x[[1]]
+    ns <- x[[2]]
+  }
+
+  # Error -
+
+  query(f, ns, warn)
+}
+
+query <- function(f, ns, warn = FALSE){
+  #searches ns for f, prints.
+  print()
+}
