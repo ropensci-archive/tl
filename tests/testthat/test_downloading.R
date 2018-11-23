a0c62cb227aebb40eda109e649ca5417510e6508
Holly Kirk <hollylkirk@gmail.com>
2018-11-23 13:06:45 +1100

    fixes test for downloading function

--- tests/testthat/test_downloading.R
+++ tests/testthat/test_downloading.R
@@ -2,10 +2,10 @@ context("downloading")
 
 test_that("a page can be downloaded", {
 
-  target <- paste0("# graphics::plot\n\n> base plotting\n\n",
-                   "  - dot plot of y against x\n  \n  ",
-                   "`plot (y ~ x)`\n\n  - line plot of y against",
-                   " x\n  \n  `plot (y ~ x, type = \"l\")`")
+  target <- paste0("# graphics::plot\n\nbase plotting\n\n- ",
+                   "dot plot of y against x  \n",
+                   "`plot (y ~ x)`\n\n- line plot of y against",
+                   " x  \n`plot (y ~ x, type = \"l\")`")
   page <- get_page("graphics", "plot")
   expect_equal(page, target)
 
